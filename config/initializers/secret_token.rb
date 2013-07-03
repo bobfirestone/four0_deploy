47ef81400f8ec29de3a900cc66083ddfbea3524a
Bob Firestone <bob@rbf.me>
2013-07-03 11:20:24 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Four0Deploy::Application.config.secret_key_base = '356e16271a16c0a896b9ae7eaeee522334320024ec214c130d5ff7f0edb5b049f82cfa28cec7b2d0b3054ee25ebb4d7986d972a23a9180d6ce13aa8ea575d23e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Four0Deploy::Application.config.session_store :cookie_store, key: '_four0_deploy_session'
